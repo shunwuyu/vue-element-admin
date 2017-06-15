677011bb08a85e6a3499cf63cd85f916e3f4a166
喻顺武 <shunwu2001@163.com>
2017-06-15 10:22:20 +0800

    first commit

---
+++ src/views/layout/Layout.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+<div class="app-wrapper">
+  <div class="sidebar-wrapper">
+
+  </div>
+  <router-view/>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
